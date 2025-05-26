d6074a06bc8e9e2377e6d24ffcf362e72817bd59
razikdontcare <gamersia4@gmail.com>
2025-05-26 21:10:09 +0800

    [server]: fix(database): ensure port is included in MySQL pool configuration

--- server/src/lib/mysql.ts
+++ server/src/lib/mysql.ts
@@ -6,6 +6,7 @@ const dbConfig: mysql.PoolOptions = {
   user: env.DB_USER,
   password: env.DB_PASSWORD,
   database: env.DB_NAME,
+  port: env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
