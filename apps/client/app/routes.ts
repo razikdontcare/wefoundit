b379809d6de46be690552231145f46b1167ad245
Komang Krisna Jaya Nova Antara <krisnajayanova66@gmail.com>
2025-06-18 21:53:33 +0800

    [client]: feat(routes): add search route to main layout

--- apps/client/app/routes.ts
+++ apps/client/app/routes.ts
@@ -1,5 +1,8 @@
-import { type RouteConfig, index, layout } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
-  layout("./layouts/main-layout.tsx", [index("./routes/home.tsx")]),
+  layout("./layouts/main-layout.tsx",[
+    index("./routes/home.tsx"),
+    route("/search", "./routes/search.tsx"),
+  ]),
 ] satisfies RouteConfig;
