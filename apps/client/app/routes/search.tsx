595b35b633709bdfad444b18b4065c73a24879f0
Komang Krisna Jaya Nova Antara <krisnajayanova66@gmail.com>
2025-06-19 19:28:56 +0800

    [client]: style(ui): apply lowercase styling to search results and title

--- apps/client/app/routes/search.tsx
+++ apps/client/app/routes/search.tsx
@@ -14,10 +14,10 @@ export default function Search() {
     return (<>
         <div className="flex flex-col margin px-30">
             <div className="flex justify-between py-7">
-                <h1 className="text-3xl font-bold mb-4 underline">{query}</h1>
+                <h1 className="text-3xl font-bold mb-4 underline lowercase">{query}</h1>
                 <Dropdown placeHolder="Filter" options={[{ label: "Found", labelValue: "Found"}, { label: "Lost", labelValue: "Lost"}, { label: "Reset Filter", labelValue: "Filter"}]} />
             </div>
-            <div className="container mx-auto max-w-7xl grid grid-cols-3 gap-4">
+            <div className="flex items-center justify-center container mx-auto max-w-7xl grid grid-cols-3 gap-4">
                 {Array.from({ length: 10 }).map((_, index) => (
                     <div key={index}>
                     <Card />
@@ -26,7 +26,7 @@ export default function Search() {
             </div>
             <div className="flex justify-center items-center mt-10 py-4">
                 <p className="text-center text-gray-600 place-content-end">
-                    Search results for: <span className="font-semibold">{query}</span>
+                    Search results for: <span className="font-semibold lowercase">{query}</span>
                 </p>
             </div>
         </div>
