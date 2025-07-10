ba8a6322fedf3231840abff0d07abe09bc690a0d
Komang Krisna Jaya Nova Antara <krisnajayanova66@gmail.com>
2025-07-10 20:01:46 +0800

    [client]: feat(auth): implement registration details component

--- apps/client/app/routes/auth.tsx
+++ apps/client/app/routes/auth.tsx
@@ -2,6 +2,7 @@ import type { Route } from "./+types/auth";
 import { useNavigate } from "react-router";
 import Login from "~/components/signin";
 import Register from "~/components/signup";
+import RegisterDetails from "~/components/signup-details";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "WeFoundIt" }, { name: "description", content: "" }];
@@ -12,7 +13,7 @@ export default function Auth() {
   return (
     <>
         <div className="flex flex-row min-h-screen bg-[#1C1F23]">
-            <Register />
+            <RegisterDetails />
         </div>
     </>
   );
